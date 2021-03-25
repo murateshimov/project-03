84fabdb95f09802f0db33de62ec40b06d73ec756
murateshimov <57397667+murateshimov@users.noreply.github.com>
2021-03-25 19:49:26 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form</title>
+</head>
+<body>
+	<center>
+		<h3>Login Here</h3>
+		<form action = "login.php" method = "post">
+			<table>
+
+				<tr>
+					<td>Login:</td>
+					<td>
+						<input type="text" name="user">
+					</td>
+				</tr>
+
+				<tr>
+					<td>Password:</td>
+					<td>
+						<input type="password" name="1234">
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<input type="reset" name="SUBMIT" VALUE = "CLEAR">
+					</td>
+					<td>
+						<input type="submit" name="SUBMIT" >
+					</td>
+					
+				</tr>
+
+			</table>
+			
+		</form>
+	</center>
+
+</body>
+</html>
