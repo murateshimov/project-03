f97fbbb0373481d79affa2a48613d9df583e1809
murateshimov <57397667+murateshimov@users.noreply.github.com>
2021-03-26 10:24:39 +0600

    Update index.php

--- index.php
+++ index.php
@@ -12,27 +12,40 @@
 				<tr>  
 					<td>Login:</td>
 					<td>
-						<input type="text" name="murat" required="" onclick="checkPswd();">
+						<input type="text" name="login" required="" id = 'login'>
 					</td>
 				</tr>
 
 				<tr>
 					<td>Password:</td>
 					<td>
-						<input type="password" name="pwd" required="" >
+						<input type="password" name="password" required="" id = "pwd">
 					</td>
 				</tr>
 			</table>
 				
 				<button type="reset" class="clear_button" value="clear">Clear</button>
 				<form>
-         <button type="submit" formaction="https://murat-cv.herokuapp.com/">Submit</button>
+         <button type="submit" formaction="https://murat-cv.herokuapp.com/" id = "submit_button">Submit</button>
       </form>
 				
 			
 			
 		</form>
 	</center>
+	<script>
+		document.forms[0].onsubmit = function (e){
+		let login = document.getElementById('login').value;
+		let password = document.getElementById('pwd').value;
+			
+		if (login === "murat" && password === "1234")
+			alert ("welcome");
+		else{
+			alert("Invalid Password or login");
+		}
+		};
+	<script/>
+
 
 </body>
 </html>
